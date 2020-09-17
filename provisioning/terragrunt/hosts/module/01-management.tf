9a72973c74d103302431f64543b51cef73ef2e70
Maximilian Frank <maximilian.frank@ait.ac.at>
2020-09-17 22:54:19 +0200

    Fix user data issue and volume_size forward missing

--- provisioning/terragrunt/hosts/module/01-management.tf
+++ provisioning/terragrunt/hosts/module/01-management.tf
@@ -1,11 +1,12 @@
 module "mgmthost" {
   count              = var.mgmthost_active ? 1 : 0
-  source             = "git@git-service.ait.ac.at:sct-cyberrange/terraform-modules/openstack-srv_noportsec.git?ref=v1.3"
+  source             = "git@git-service.ait.ac.at:sct-cyberrange/terraform-modules/openstack-srv_noportsec.git?ref=v1.3.1"
   hostname           = "mgmthost"
-  tag                = "mgmthost"
+  tag                = "management"
   host_address_index = var.mgmthost_ip_index
   image              = local.mgmthost_image
   flavor             = local.mgmthost_flavor
+  volume_size        = local.mgmthost_volume_size
   sshkey             = var.sshkey
   network            = var.internet
   subnet             = var.internet_subnet

282d0f5ad909e0201152ff14b6bb7df3b7a7e4dd
Maximilian Frank <maximilian.frank@ait.ac.at>
2020-10-24 18:35:07 +0200

    Add ext dns server and make firewall local dns

---
+++ provisioning/terragrunt/bootstrap/module/main.tf
@@ -0,0 +1,44 @@
+terraform {
+  backend "consul" {}
+}
+
+locals {
+  ext_dns_userdata_file = var.ext_dns_userdata == null ? "${path.module}/scripts/default.yml" : var.ext_dns_userdata
+  host_userdata_file = var.host_userdata == null ? "${path.module}/scripts/firewallinit.yml" : var.host_userdata
+}
+
+module "vmnets" {
+  source = "git@git-service.ait.ac.at:sct-cyberrange/terraform-modules/openstack-vmnets.git?ref=v1.4"
+  host_name = var.host_name
+  host_image = var.host_image
+  host_size = var.host_size
+  host_flavor = var.host_flavor
+  host_tag = var.host_tag
+  host_ext_address_index = var.host_ext_address_index
+  host_userdata = local.host_userdata_file
+  ext_subnet = var.ext_subnet
+  extnet = var.extnet
+  extnet_create = true
+  ext_cidr = var.ext_cidr
+  ext_dns = concat([cidrhost(var.ext_cidr, var.ext_dns_address_index)], var.ext_additional_dns )
+  router_name = var.router_name
+  sshkey = var.sshkey
+  floating_ip_pool = var.floating_ip_pool
+  networks = var.networks
+}
+
+module "internet_dns" {
+  count         = var.extnet_create  ? 1 : 0
+  source        = "git@git-service.ait.ac.at:sct-cyberrange/terraform-modules/openstack-srv_noportsec.git?ref=v1.3.2"
+  hostname      = var.ext_dns_name
+  tag           = var.ext_dns_tag
+  host_address_index = var.ext_dns_address_index
+  image         = var.ext_dns_image
+  flavor        = var.ext_dns_flavor
+  volume_size   = var.ext_dns_size
+  sshkey        = var.sshkey
+  network       = module.vmnets.extnet.network
+  subnet        = module.vmnets.extnet.subnet
+  userdatafile  = local.ext_dns_userdata_file
+  userdata_vars = var.ext_dns_userdata_vars
+}
--- provisioning/terragrunt/bootstrap/terragrunt.hcl
+++ provisioning/terragrunt/bootstrap/terragrunt.hcl
@@ -1,16 +1,27 @@
 terraform {
-    source = "git@git-service.ait.ac.at:sct-cyberrange/terraform-modules/openstack-vmnets.git?ref=v1.2"
+    source = ".//module"
 }
 
 inputs = {
+    # firewall host
     host_name = "inet-firewall"
     host_image = "aecid-ubuntu-bionic-amd64"
     host_tag = "firewall"
-    host_ext_ip = "100"
-    host_userdata = "firewallinit.yml"
+    host_ext_address_index = 42420
+    host_size  = 40
+
+    # external dns server
+    ext_dns_name = "inet-dns"
+    ext_dns_image = "aecid-ubuntu-bionic-amd64"
+    ext_dns_tag = "internet, dnservers"
+    ext_dns_ext_address_index = 22
+    ext_dns_size = 20
+
     ext_subnet = "internet-subnet"
     extnet = "internet"
     extnet_create = true
+    ext_dns_address_index = 13370
+    ext_additional_dns = ["8.8.8.8"]
     router_name = "aecid-testbed-router"
     sshkey = "testbed-key"
     floating_ip_pool = "provider-aecid-208"
@@ -20,14 +31,16 @@ inputs = {
                 host_address_index = "1",
 		subnet = "local-subnet", 
 		cidr = "172.16.0.0/24", 
-                dns: ["8.8.8.8"] 
+                dns = ["8.8.8.8"],
+                host_as_dns = true
 	}
         dmz = {
                 network = "dmz",
                 host_address_index = "1",
 		subnet = "dmz-subnet"
 		cidr = "172.16.100.0/24"
-                dns: ["8.8.8.8"] 
+                dns = ["8.8.8.8"],
+                host_as_dns = true
         }
     }
 }

4f8bdb2bc6e3b66dcf9a2438fdffcf2ee00457f4
landauermax <max.landauer@ait.ac.at>
2021-09-21 07:24:01 +0200

    Add logstash config template files

--- provisioning/ansible/run_all.sh
+++ provisioning/ansible/run_all.sh
@@ -1,73 +1,25 @@
-ansible-playbook ./playbooks/deploy/dns/main.yml
-if [[ $? -ne 0 ]]; then
+run_playbook () {
+  ansible-playbook $1
+  if [[ $? -ne 0 ]]; then
     exit
-fi
-ansible-playbook ./playbooks/deploy/firewall/main.yml
-if [[ $? -ne 0 ]]; then
-    exit
-fi
-ansible-playbook ./playbooks/deploy/certs/main.yml
-if [[ $? -ne 0 ]]; then
-    exit
-fi
-ansible-playbook ./playbooks/deploy/ssh_keys/main.yml
-if [[ $? -ne 0 ]]; then
-    exit
-fi
-ansible-playbook ./playbooks/deploy/employees/main.yml
-if [[ $? -ne 0 ]]; then
-    exit
-fi
-ansible-playbook ./playbooks/deploy/ext_user/main.yml
-if [[ $? -ne 0 ]]; then
-    exit
-fi
-ansible-playbook ./playbooks/deploy/mailservers/main.yml
-if [[ $? -ne 0 ]]; then
-    exit
-fi
-ansible-playbook ./playbooks/deploy/webserver/main.yml
-if [[ $? -ne 0 ]]; then
-    exit
-fi
-ansible-playbook ./playbooks/deploy/wordpress/main.yml
-if [[ $? -ne 0 ]]; then
-    exit
-fi
-ansible-playbook ./playbooks/deploy/samba_shares/main.yml
-if [[ $? -ne 0 ]]; then
-    exit
-fi
-ansible-playbook ./playbooks/deploy/owncloud/main.yml
-if [[ $? -ne 0 ]]; then
-    exit
-fi
-ansible-playbook ./playbooks/deploy/gather/main.yml
-if [[ $? -ne 0 ]]; then
-    exit
-fi
-ansible-playbook ./playbooks/deploy/attack_server_takeover/main.yml
-if [[ $? -ne 0 ]]; then
-    exit
-fi
-ansible-playbook ./playbooks/deploy/openvpn/main.yml
-if [[ $? -ne 0 ]]; then
-    exit
-fi
-ansible-playbook ./playbooks/deploy/attack_exfiltration/main.yml
-if [[ $? -ne 0 ]]; then
-    exit
-fi
-ansible-playbook ./playbooks/deploy/kyoushi_simulation/main.yml
-if [[ $? -ne 0 ]]; then
-    exit
-fi
-ansible-playbook ./playbooks/deploy/monitoring/main.yml
-if [[ $? -ne 0 ]]; then
-    exit
-fi
-ansible-playbook ./playbooks/deploy/monitoring/beats.yml
-if [[ $? -ne 0 ]]; then
-    exit
-fi
-ansible-playbook ./playbooks/deploy/audit/main.yml
+  fi
+}
+run_playbook ./playbooks/deploy/dns/main.yml
+run_playbook ./playbooks/deploy/firewall/main.yml
+run_playbook ./playbooks/deploy/certs/main.yml
+run_playbook ./playbooks/deploy/ssh_keys/main.yml
+run_playbook ./playbooks/deploy/employees/main.yml
+run_playbook ./playbooks/deploy/ext_user/main.yml
+run_playbook ./playbooks/deploy/mailservers/main.yml
+run_playbook ./playbooks/deploy/webserver/main.yml
+run_playbook ./playbooks/deploy/wordpress/main.yml
+run_playbook ./playbooks/deploy/samba_shares/main.yml
+run_playbook ./playbooks/deploy/owncloud/main.yml
+run_playbook ./playbooks/deploy/gather/main.yml
+run_playbook ./playbooks/deploy/attack_server_takeover/main.yml
+run_playbook ./playbooks/deploy/openvpn/main.yml
+run_playbook ./playbooks/deploy/attack_exfiltration/main.yml
+run_playbook ./playbooks/deploy/kyoushi_simulation/main.yml
+run_playbook ./playbooks/deploy/monitoring/main.yml
+run_playbook ./playbooks/deploy/monitoring/beats.yml
+run_playbook ./playbooks/deploy/audit/main.yml
